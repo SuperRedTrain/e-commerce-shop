34877a508939bc9be0a451c6d4729118b2b7ec44
Ke Shao <kshao1@emich.edu>
2021-10-15 14:41:33 +0000

    Fixed bugs in product.js
    Remove html tags from description of products. Add all images for a product.

--- src/components/products/product.js
+++ src/components/products/product.js
@@ -1,39 +1,52 @@
 import { useParams } from "react-router";
 import { useState, useEffect} from "react";
-// import {stripHtml} from "string-strip-html";
 import { commerce } from '../../lib/commerce';
-
 
 
 function Product() {
 
     const { productId } = useParams();
-    const [product, setProduct] = useState([]);
- //   const {prodDespcription} = stripHtml(product.description);
-
-
-
+    const [product, setProduct] = useState(undefined);
 
     useEffect(() => {
         commerce.products.retrieve(productId).then(result => {
             console.log(result);
             setProduct(result);
         });
-
     }, [productId]) 
 
+   
     return(
-        <div>
-           
+        
+        <div>  
+            {  product &&
+                <div>                  
             <h2>{product.name}</h2>
-{/*             <div>
-                <img src={product.image.url} alt={product.name} height="300" /> 
-            </div> */}
+        
+            <h3>price: {product.price.formatted_with_symbol}</h3>
+
+            <div>
+{/*                 <img src={product.image.url} alt={product && product.name} height="300" /> 
+                <img src={product.assets[1].url} alt='image1' height="300" />  */}
+                {
+                    product.assets.map((picture) =>{
+
+                    return  <img key={picture.id} src={picture.url} alt={picture.id} height="300" />         
+                    })
+
+                } 
+               
+            </div> 
             
-            <div>Description: {product.description}</div>
+            <div>
+                <h3>Description:</h3> 
+                {product && product.description.replace(/(<([^>]+)>)/ig, ' ')} 
+            </div>  
+
+
+            </div>  
+            } 
         </div>
     )
-
-
 }
 export default Product;
